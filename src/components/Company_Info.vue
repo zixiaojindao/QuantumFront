3a921aeb75ab731932ba3d4046271d85fb41b1c8
jason <wangyin844507546@users.noreply.github.com>
2018-12-02 23:12:20 +0800

    home

--- src/components/Company_Info.vue
+++ src/components/Company_Info.vue
@@ -1,5 +1,152 @@
 <template>
-<div></div>
+        <div class="wrapper wrapper-content animated fadeInRight">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>公司介绍</h5>
+                        </div>
+                        <div class="ibox-content">
+                            <table class="footable table table-stripped toggle-arrow-tiny" data-page-size="8">
+                                <thead>
+                                   
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="col-md-2">公司名称</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">曾用名</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">英文名称</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">公司简介</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">经营范围</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">国籍</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">成立日期</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">统一社会信用代码</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">注册资本</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">货币资本</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">法定代表人</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">实际控制人</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">第一大股东</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">第一大股东(持股比例)</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">主营构成(最新年报)</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">所属证监会行业</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">所属Wind行业</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">所属Wind主题行业</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">员工总数</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">总经理</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">董事会秘书</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">省份</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">城市</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">注册地址</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">办公地址</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">邮编</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">电话</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">传真</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">电子邮件</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="col-md-2">公司网站</td>
+                                        <td  class="col-md-10">万科企业股份邮箱公司</td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="5">
+                                        <ul class="pagination pull-right"></ul>
+                                    </td>
+                                </tr>
+                                </tfoot>
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </template>
 <script>
 export default {
--- src/components/Credit_Report.vue
+++ src/components/Credit_Report.vue
@@ -1,5 +1,114 @@
 <template>
-<div></div>
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h5>总体摘要</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <h5>万科企业股份有限公司</h5>
+                        <div class="ibox-content">
+                            <table class="table table-bordered" >
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>胜遇评级</th>
+                                        <th>公开市场评级</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>信用评级</td>
+                                        <td>AA</td>
+                                        <td>AAA</td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="ibox-content">
+                        <span>总体摘要:</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h5>信用评分明细</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>2018/3/31</th>
+                                        <th>2017/12/31</th>
+                                        <th>2017/9/30</th>
+                                        <th>2017/9/30</th>
+                                        <th>2017/9/30</th>
+                                        <th>2017/9/30</th>
+                                        <th>2017/9/30</th>
+                                        <th>2017/9/30</th>
+                                        <th>2017/9/30</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>盈利能力</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                        <td>3.20</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h5>财务比率</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover dataTables-example" >
+                                <thead>
+                                    <tr>
+                                        <th>报告期</th>
+                                        <th>总资产(亿元)</th>
+                                        <th>总负债(亿元)</th>
+                                        <th>资产负债率</th>
+                                        <th>毛利率</th>
+                                        <th>净利率</th>
+                                        <th>总资产受益率</th>
+                                        <th>总资产受益率</th>
+                                        <th>总资产受益率</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                                <tfoot>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
